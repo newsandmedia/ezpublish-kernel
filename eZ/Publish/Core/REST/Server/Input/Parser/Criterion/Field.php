67ff35cdf958ed6cf47cc730c0860018627afe7a
oleksandr.peshkov <oleksandr.peshkov@newsandmedia.sk>
2017-03-13 12:27:33 +0100

    EZPLATFORM-1465 Fixed "field" criterion for filtering through API

--- eZ/Publish/Core/REST/Server/Input/Parser/Criterion/Field.php
+++ eZ/Publish/Core/REST/Server/Input/Parser/Criterion/Field.php
@@ -10,6 +10,8 @@ namespace eZ\Publish\Core\REST\Server\Input\Parser\Criterion;
 
 use eZ\Publish\Core\REST\Common\Input\BaseParser;
 use eZ\Publish\Core\REST\Common\Input\ParsingDispatcher;
+use eZ\Publish\Core\REST\Common\Exceptions;
+use eZ\Publish\API\Repository\Values\Content\Query\Criterion\Field as FieldCriterion;
 
 /**
  * Parser for Field Criterion.
@@ -28,6 +30,28 @@ class Field extends BaseParser
      */
     public function parse(array $data, ParsingDispatcher $parsingDispatcher)
     {
-        throw new \Exception('@todo implement');
+        if (!isset($data['FieldCriterion'])) {
+            throw new Exceptions\Parser('Invalid <FieldCriterion> format');
+        }
+
+        if (!isset($data['FieldCriterion']['Target'])) {
+            throw new Exceptions\Parser('Invalid <Target> format');
+        }
+
+        $target = $data['FieldCriterion']['Target'];
+
+        if (!isset($data['FieldCriterion']['Value'])) {
+            throw new Exceptions\Parser('Invalid <Value> format');
+        }
+
+        if (!in_array(gettype($data['FieldCriterion']['Value']), ['integer', 'string', 'array'])) {
+            throw new Exceptions\Parser('Invalid <Value> format');
+        }
+
+        $value = is_array($data['FieldCriterion']['Value'])
+            ? $data['FieldCriterion']['Value']
+            : explode(',', $data['FieldCriterion']['Value']);
+
+        return new FieldCriterion($target, null, $value);
     }
 }
